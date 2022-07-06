776eac23934faebc277d8282d139f4012ee10c0c
Tomoki Aizawa <aiaiaiaidayo4412@gmail.com>
2022-07-07 01:27:11 +0900

    [□と○]を○学習しました

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,5 @@
+const figure = document.getElementById("figure")
+
+figure.onmouseover = function () {
+  figure.classList.toggle("rounded")
+}
--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,10 +1,22 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minasButton = document.getElementById("minas-button")
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 
 plusButton.onclick = function () {
   count += 1
 
+  display.textContent = count
+}
+
+minasButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+
+doubleButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
