c5d9bf00ce051681be1424b96758aeb0f1808c00
Tomoki Aizawa <aiaiaiaidayo4412@gmail.com>
2022-07-06 23:56:49 +0900

    [カウンター]を学習しました

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("button")
+
+button.onclick = function () {
+  alert("クリックしたね")
+}
+// input 要素と input イベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (a) {
+  console.log(a.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
